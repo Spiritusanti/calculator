4d32553c6a68957146eed280b68b752db13b66da
Spiritusanti <mccrackenjacob@gmail.com>
2021-06-02 14:22:19 -0700

    removed title component and added event listeners for numbers, clear, and decimal buttons

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const calculatorDisplay = document.getElementById('calc-content');
-const inputBtns = document.querySelectorAll('button');
-const clearBtn = document.getElementById('clear-btn');
+const calculatorDisplay = document.getElementById("calc-content");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
 
-function sendNumberValue(number) {
-    console.log(number);
+function sendNumberValue(number) { 
+    // if current display value is 0, replaceit, if not add number
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal() {
+    // if no decimal, add one
+    if(!calculatorDisplay.textContent.includes('.')) {
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+    }
+}
 
+// Add Event Listeners for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => addDecimal());
+  } else {
+    return;
+  }
+});
 
-// EventListeners
+// reset display
+function resetAll() {
+    calculatorDisplay.textContent = '0';
+}
+
+// Event listener
+clearBtn.addEventListener('click', resetAll);
